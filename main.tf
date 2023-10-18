c2e8e39ffbf6100148d018286c3f99437b89df5e
Anthony Igwe <igweanthony15@gmail.com>
2023-10-18 20:08:34 +0000

    #15 migrate local state to terraform cloud using terraform cloud block

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
 terraform {
+  #backend "remote" {
+  # hostname = "app.terraform.io"
+  #  organization = "Terraform-beginners-bootcamp-2023"
+
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+cloud {
+    organization = "Terraform-beginners-bootcamp-2023"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
