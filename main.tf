204eceef82f8996844d684fdfffefff510ea244d
AnthonyIgwe1 <105819094+AnthonyIgwe1@users.noreply.github.com>
2023-11-07 12:42:20 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
-   # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower   = true
   upper   = false
   length  = 32
   special = false
 }
+
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
   # Bucket naming rules
@@ -15,4 +16,3 @@ tags = {
     UserUuid =var.user_uuid
   }
 }
-
