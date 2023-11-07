87e6fd2fc7fca9631995c122ae66a3d74af766c5
AnthonyIgwe1 <105819094+AnthonyIgwe1@users.noreply.github.com>
2023-11-07 12:37:12 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,8 @@
 variable "user_uuid" {
-  type    = string
-  default = "24eecd4c-e2ad-488d-ac62-5f116a250ee1"
+  description = "The UUID of the user"
+  type        = string
+  validation {
+    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message    = "The user_uuid value is not a valid UUID."
+  }
 }
